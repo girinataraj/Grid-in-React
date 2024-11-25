da867de47385ac2ad4d625b7e3201539422792fd
girinataraj <53966448+girinataraj@users.noreply.github.com>
2024-11-25 19:37:45 +0530

    Add files via upload
    A classic newspaper-style layout built using CSS Grid in React.js. This project demonstrates how to create a fixed-width, visually structured design for headlines, articles, and images without using responsive techniques.
    
    Key Highlights:
    
    CSS Grid Layout: Efficient grid system for rows and columns, perfect for creating a traditional newspaper look.
    React Integration: Modular components for clean and maintainable code.
    Non-Responsive Design: Fixed-width approach for simplicity and focus on desktop-first layouts.

---
+++ Components/Task.tsx
@@ -0,0 +1,148 @@
+import React from "react";
+import "./Task.css"
+
+function Task()
+{
+    return(
+        <article>
+            <header className="headder">
+                <div className="hb1"><p>Quis nostrud exercitstio nullamco laboris</p></div>
+                <div className="hb2"><h1 className="">Newspaper</h1></div>
+                <div className="hb3"><h3>Lorem ipsum</h3><p>dolor, sit amet consectetur adipisicing elit. cumque provident, sed esse eligendi vel.</p></div>
+            </header>
+            <div className="datediv">FRIDAY, OCTOBER 19, 2018</div>
+            <div className="maintitle"><h1>LOREM IPSUM DOLOR</h1></div>
+            <div className="subtitle"><h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem vitae nam maxime modi labore doloremque itaque quas consequatur</h1></div>
+            <section className="mainbody">
+                <div className="bodyleft">
+                    <div className="LC1">
+                        <div className="imgdiv1"></div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsa ut quibusdam cupiditate consectetur non debitis dignissimos delectus ducimus, quas atque earum ea excepturi! Vitae unde molestias nostrum laborum incidunt.</p>
+                    </div>
+                    <div className="leftsplit">
+                        <div className="LSl">
+                            <h1>Lorem ipsum, dolor sit amet</h1>
+                            <hr />
+                            <h2> consectetur adipisicing elit</h2>
+                            <hr />
+                            <p>
+                            . Error tempore unde natus totam nihil perspiciatis, odit officia consequatur, quisquam non ex laboriosam amet, necessitatibus molestiae cumque sed quidem animi dolores?   
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo tempora molestiae, quos vero similique nihil. Commodi numquam ipsam ea rerum. Cumque quisquam tenetur iste porro modi inventore voluptates dolore atque!
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam ducimus sit, recusandae repellendus, beatae voluptates esse asperiores blanditiis tempora architecto incidunt quo corporis illo perferendis maiores perspiciatis, quaerat voluptatem libero?
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum consequatur explicabo officia, minima voluptas temporibus dignissimos debitis autem aliquam, nam tempora tempore sunt earum minus porro atque quae, doloremque aut.
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum consequatur explicabo officia, minima voluptas temporibus dignissimos debitis autem aliquam, nam tempora tempore sunt earum minus porro atque quae, doloremque aut.
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum consequatur explicabo officia, minima voluptas temporibus dignissimos debitis autem aliquam, nam tempora tempore sunt earum minus porro atque quae, doloremque aut.
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum consequatur explicabo officia, minima voluptas temporibus dignissimos debitis autem aliquam, nam tempora tempore sunt earum minus porro atque quae, doloremque aut.
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum, asperiores deleniti voluptatem voluptates saepe nostrum debitis. Culpa ipsum in corrupti sint dolorum magni possimus, quia harum quibusdam totam repellendus nostrum.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam minus, molestias perspiciatis provident pariatur placeat hic incidunt non possimus vel obcaecati? Quaerat ea maxime quas explicabo aspernatur, sit facilis dignissimos.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla soluta distinctio odit asperiores repellendus totam suscipit iure perferendis autem quo unde, sapiente magni in esse rerum harum dolorum cum possimus?
+                            </p>
+                        </div>
+                        <div className="LSr">
+                            <h1>Duis aute irure dolor in reprehenderit</h1>
+                            <hr />
+                            <div className="paradiv">
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum aspernatur ipsum velit aliquam. Autem rerum amet tenetur eaque eligendi neque. Eius sequi repudiandae, magnam dolorum animi temporibus illum delectus sunt. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate iusto ex quidem, temporibus, ad pariatur amet dolorum, a modi aspernatur odio alias blanditiis sequi. Eaque iure unde fuga officia possimus.</p>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum aspernatur ipsum velit aliquam. Autem rerum amet tenetur eaque eligendi neque. Eius sequi repudiandae, magnam dolorum animi temporibus illum delectus sunt. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate iusto ex quidem, temporibus, ad pariatur amet dolorum, a modi aspernatur odio alias blanditiis sequi. Eaque iure unde fuga officia possimus.</p>
+                                
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum aspernatur ipsum velit aliquam. Autem rerum amet tenetur eaque eligendi neque. Eius sequi repudiandae, magnam dolorum animi temporibus illum delectus sunt. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate iusto ex quidem, temporibus, ad pariatur amet dolorum, a modi aspernatur odio alias blanditiis sequi. Eaque iure unde fuga officia possimus.</p>
+                            </div>
+                            <hr />
+                            <h3>Duis aute irure</h3>
+                            <hr />
+                            <div className="paradiv">
+                                <div>
+                                    <h1>Occaecat cupidatat</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio cumque laborum quod ducimus. Totam doloribus temporibus eligendi ex natus eveniet, eius ratione, ea, minus recusandae veniam reiciendis explicabo nostrum quos?</p>
+                                    <h1>Cillum dolore</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi molestiae hic voluptates totam, tenetur commodi deserunt optio pariatur accusamus consequatur dolorem nobis minima! Obcaecati fugiat veritatis nulla? Maiores, ipsum a.</p>
+                                    <h1>Duis aute</h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem dolorem obcaecati iusto at possimus harum excepturi, error non iste commodi quasi vitae quam sed natus voluptate aut. Nam, molestiae magni.</p>
+                                </div>
+                                <div>
+                                    <h1>Exercitation ullameo</h1>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero repellendus eveniet accusantium. Itaque labore aspernatur sed ipsum! Nulla, provident exercitationem voluptatibus iusto fugiat voluptatem magnam sit aperiam optio distinctio perspiciatis. Lorem ipsum dolor sit amet consectetur adipisicing</p>
+                                    <h1>Incididunt ut</h1>
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni dolorum repudiandae mollitia tempora nobis ex? Quod quam quo quaerat, architecto numquam eaque nemo est quisquam iusto eligendi. Nam, odio explicabo. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit esse fugit iure totam officia eum quis saepe mollitia, dignissimos nobis aspernatur alias harum. Optio expedita asperiores minus, nesciunt amet explicabo.</p>
+                                </div>
+                                <div>
+                                    <div className="imgdiv2"></div>
+                                    <h1>Eu fugiat</h1>
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt deleniti quae velit ab aspernatur at reiciendis cupiditate quos expedita voluptatem? Culpa nostrum mollitia perspiciatis dolorem nihil molestiae, ex error debitis?
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio voluptas in vitae quam ut sint a deserunt ex nisi? Quisquam consequatur numquam excepturi nemo quidem harum vel optio, eos quibusdam.
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, quia pariatur quas, eligendi optio, incidunt ea eaque in.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bodyright">
+                    <hr />
+                    <h1>Excepteur sint occaecat cupidatat non</h1>
+                    <hr />
+                    <div className="paradiv2">
+                        <div>
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius sequi doloribus magnam quasi consequuntur perspiciatis tenetur pariatur minima vel ut quidem, atque, nobis unde aut. Nisi vero eveniet perspiciatis quod.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas reiciendis perferendis doloribus in optio nulla expedita iusto molestias cum totam, laboriosam voluptates accusamus harum repellendus ducimus inventore quod voluptatibus at.
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aperiam inventore blanditiis soluta ducimus ad magnam vel enim, excepturi ea ipsa, at cupiditate aliquam facere voluptatem consectetur aliquid omnis aut?
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aperiam inventore blanditiis soluta ducimus ad magnam vel enim, excepturi ea ipsa, at cupiditate aliquam facere voluptatem consectetur aliquid omnis aut?
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aperiam inventore blanditiis soluta ducimus ad magnam vel enim, excepturi ea ipsa, at cupiditate aliquam facere voluptatem consectetur aliquid omnis aut?
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit dolore veniam optio fugiat doloremque, nihil et veritatis nostrum, voluptates aliquid omnis in amet voluptatem, impedit ratione laboriosam quibusdam. Amet, dignissimos.
+                            </p>
+                        </div>
+                        <div>
+                            <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aperiam inventore blanditiis soluta ducimus ad magnam vel enim, excepturi ea ipsa, at cupiditate aliquam facere voluptatem consectetur aliquid omnis aut?
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aperiam inventore blanditiis soluta ducimus ad magnam vel enim, excepturi ea ipsa, at cupiditate aliquam facere voluptatem consectetur aliquid omnis aut?
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aperiam inventore blanditiis soluta ducimus ad magnam vel enim, excepturi ea ipsa, at cupiditate aliquam facere voluptatem consectetur aliquid omnis aut?
+                            
+                            </p>
+                            <div className="imgdiv3"></div>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, suscipit debitis nulla fuga quia aliquid odio exercitationem hic, officia voluptatem voluptate veniam, ullam sunt cum eaque adipisci? Repellat, enim dolorum?</p>
+                        </div>
+                    </div>
+                    <hr />
+                    <h1>Sunt in culpa qui officia deserunt</h1>
+                    <hr />
+                    <div className="paradiv2">
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ex itaque minus vero voluptate perspiciatis, quisquam ad facilis sint ducimus pariatur quasi corrupti soluta ratione. Adipisci voluptatibus facilis asperiores laudantium?
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt hic corrupti eveniet amet suscipit qui optio, voluptatem explicabo accusamus facilis corporis similique commodi! Unde laudantium, nihil dolore molestiae modi ad?
+                            </p>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ex itaque minus vero voluptate perspiciatis, quisquam ad facilis sint ducimus pariatur quasi corrupti soluta ratione. Adipisci voluptatibus facilis asperiores laudantium?
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt hic corrupti eveniet amet suscipit qui optio, voluptatem explicabo accusamus facilis corporis similique commodi! Unde laudantium, nihil dolore molestiae modi ad?
+                            </p>
+                        </div>
+                    </div>
+                    <hr />
+                    <h3>Excepteur</h3>
+                    <hr />
+                    <div className="paradiv2">
+                        <div>
+                            <h1>Lorem ipsum dolor sit amet</h1>
+                            <p>consectetur, adipisicing elit. Nesciunt hic corrupti eveniet amet suscipit qui optio, voluptatem explicabo accusamus facilis corporis similique commodi! Unde laudantium, nihil dolore molestiae modi
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae iste dolor quasi voluptatum, sint accusamus, tenetur ab iusto saepe ducimus dolore quod illum eveniet officiis magnam doloremque harum qui natus?
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                        <div>
+                            <h1>Lorem ipsum dolor sit amet</h1>
+                            <p>consectetur, adipisicing elit. Nesciunt hic corrupti eveniet amet suscipit qui optio, voluptatem explicabo accusamus facilis corporis similique commodi! Unde laudantium, nihil dolore molestiae modi
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae iste dolor quasi voluptatum, sint accusamus, tenetur ab iusto saepe ducimus dolore quod illum eveniet officiis magnam doloremque harum qui natus?
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </article>
+    );
+}
+
+export default Task
